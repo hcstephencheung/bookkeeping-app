fec9b1e6f693779105a7476eb0698ee358366770
Stephen Cheung <stephencheung@Stephens-Air-2.hitronhub.home>
2017-01-19 01:35:12 -0800

    Added util to make dates readable; Styled font smaller in table

--- js/app.js
+++ js/app.js
@@ -57,8 +57,59 @@ var Utils = (function() {
         return (amount > 0) ? '$' + amount : '-$' + Math.abs(amount);
     };
 
+    var convertDateToReadable = function(YYYYMMDD) {
+        if (!YYYYMMDD) {
+            console.warn('Utils.convertDateToReadable : input date is undefined', YYYYMMDD);
+            return;
+        }
+
+        if (!/\d{4}\-\d{2}\-\d{2}/.test(YYYYMMDD)) {
+            console.warn('Utils.convertDateToReadable : input date format is incorrect, please follow YYYY-MM-DD', YYYYMMDD);
+            return;
+        }
+
+        var months = ['boom', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        var suffix = [undefined, 'st', 'nd', 'rd'];
+        var delim = '-';
+        var convertedDate = {};
+
+        YYYYMMDD.split('-').map(function(ymd, idx) {
+            var ymdNum = parseInt(ymd);
+            var ymdString = '' + ymd; // ensured string, for suffix logic
+
+            switch (idx) {
+                case 0:
+                    convertedDate.year = ymdNum;
+                    break;
+                case 1:
+                    convertedDate.month = months[ymdNum]; // 0-indexing
+                    break;
+                case 2:
+                    convertedDate.day = ymdNum;
+
+                    // English is weird...
+                    if (ymdString.match(/1\d/)) {
+                        convertedDate.suffix = 'th'
+                        break;
+                    }
+
+                    var lastDigit = (ymdString.match(/\d$/) || [0])[0];
+
+                    convertedDate.suffix = !!suffix[lastDigit] ?
+                                            suffix[lastDigit] :
+                                            'th';
+                    break;
+                default:
+                    return;
+            }
+        });
+
+        return convertedDate.month + ' ' + convertedDate.day + convertedDate.suffix + ', ' + convertedDate.year;
+    };
+
     return {
-        serializeDollar: serializeDollar
+        serializeDollar: serializeDollar,
+        convertDateToReadable: convertDateToReadable
     };
 })()
 // ---
@@ -240,7 +291,7 @@ var Components = {
             var rowEl = document.createElement('tbody');
             rowEl.innerHTML =
                 '<tr class="c-ledger__row">' +
-                '<td>' + rowData.Date + '</td>' +
+                '<td>' + Utils.convertDateToReadable(rowData.Date) + '</td>' +
                 '<td>' + rowData.Company + '</td>' +
                 '<td>' + rowData.Ledger + '</td>' +
                 '<td>' + Utils.serializeDollar(parseFloat(rowData.Amount)) + '</td>' +
